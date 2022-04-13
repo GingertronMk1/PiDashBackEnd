8f38507f29780d69d0d2b06598cb2a4b6fe6220b
Jack Ellis <jackellis1504@gmail.com>
2022-04-13 19:03:27 +0100

    Uhh kinda don't need any of the rest of it

--- assets/app.js
+++
@@ -1,8 +0,0 @@
-window.axios = require("axios");
-
-import { createApp } from "vue";
-import App from "./App.vue";
-
-createApp({}).component("BaseApp", App).mount("#app");
-
-console.log("Mounted");
--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-// webpack.mix.js
-
-let mix = require('laravel-mix');
-
-mix
-  .setPublicPath('static')
-  .js('assets/app.js', 'static')
-  .vue();
