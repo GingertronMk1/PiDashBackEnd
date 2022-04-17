bcfd24f85188b6dc756c22b5f88c35a9783ab451
Jack Ellis <jackellis1504@gmail.com>
2022-04-17 13:36:38 +0100

    Passing arguments via API

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import json
 import requests
 import re
 import os
+# install python-dotenv
 from dotenv import load_dotenv
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, jsonify
-  
+from flask import Flask, jsonify, request
+
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
-  
+
 
 load_dotenv()
 
@@ -21,11 +22,11 @@ load_dotenv()
 @app.route('/cpu')
 def getCpu():
   cpuPercents = psutil.cpu_percent(percpu=True)
-  return json.dumps(cpuPercents)
+  return jsonify(cpuPercents)
 
 @app.route('/memory')
 def getMemory():
-# Calculate memory information
+  # Calculate memory information
   memory = psutil.virtual_memory()
   return jsonify(memory)
 
@@ -36,7 +37,7 @@ def getDisk():
 
 @app.route('/temperatures')
 def getTemperatures():
-  return psutil.sensors_temperatures()
+  return jsonify(psutil.sensors_temperatures())
 
 @app.route('/transmission')
 def getTransmission():
@@ -45,23 +46,14 @@ def getTransmission():
   pattern = re.compile("X-Transmission-Session-Id: (.*?)<\/")
   match = pattern.search(initReq.text)
   headers = { "X-Transmission-Session-Id": match.group(1) }
+  arguments = json.loads(request.args.get("arguments"))
   torrentsReq = requests.post(
-      url,
-      json = {
-        "method": "torrent-get",
-        "arguments": {
-          "fields": [
-            "addedDate",
-            "id",
-            "name",
-            "eta",
-            "leftUntilDone",
-            "percentDone",
-            "rateDownload"
-          ]
-        }
-      },
-      headers=headers
+    url,
+    json={
+      "method": "torrent-get",
+      "arguments": arguments
+    },
+    headers=headers
   )
   return torrentsReq.text
 
@@ -72,10 +64,10 @@ def set_headers(response):
 
 
 
-  
+
 # main driver function
 if __name__ == '__main__':
-  
-    # run() method of Flask class runs the application 
+
+  # run() method of Flask class runs the application 
     # on the local development server.
     app.run()
